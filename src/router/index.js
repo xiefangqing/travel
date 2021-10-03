b6b042337ff5787c2a340f6fe75adceaac563a65
xiefangqing <389368595@qq.com>
2021-10-03 20:53:15 +0800

    城市路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // @是指src目录
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
 export default new Router({
   // 当你访问根路径的时候，展示HelloWorld组件的内容
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }]
 })
