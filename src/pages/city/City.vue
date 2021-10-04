eb3cda15c4d778b49e249c38ee1700267b0c9a50
xiefangqing <389368595@qq.com>
2021-10-04 09:14:38 +0800

    添加城市列表区域

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './commponents/Header'
 import CitySearch from './commponents/Search'
+import CityList from './commponents/List'
+import CityAlphabet from './commponents/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
