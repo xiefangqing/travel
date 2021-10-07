c30fbf016a4275fe23f7123166a064fc5cb5f751
xiefangqing <389368595@qq.com>
2021-10-07 15:29:27 +0800

    基础动画和header修复

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -9,16 +9,19 @@
         <div class="banner-number"><span class="iconfont banner-icon">&#xec7f;</span>{{this.bannerImgs.length}}</div>
       </div>
     </div>
-    <common-gallary
-      @close="handleGallaryClose"
-      :imgs="bannerImgs"
-      v-show="showGallary"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        @close="handleGallaryClose"
+        :imgs="bannerImgs"
+        v-show="showGallary"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/Fade'
 export default {
   name: 'DetailBanner',
   props: {
@@ -40,7 +43,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
